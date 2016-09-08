b5881576d1c190110ad463dc9ac2c43df46a0517
Dan Tasse <dan.tasse@gmail.com>
2016-09-08 15:38:00 -0700

    fixed some things

--- count_per_day.py
+++ count_per_day.py
@@ -14,16 +14,30 @@ nongeotagged = collections.Counter() # date -> count
 
 outwriter = csv.writer(open(args.outfile, 'w'))
 
+# longest_line=0
+# counter = 0
+# for line in open(args.infile):
+#     counter +=1
+#     if counter % 1000000 == 0:
+#         print counter
+#     if len(line) > longest_line:
+#         longest_line = len(line)
+# 
+# print "Found longest line, it is: %s" % str(longest_line)
+longest_line = 197990 # Found this when I ran it once before.
 counter = 0
-for line in csv.reader(open(args.infile), delimiter='\t'):
+reader = csv.reader(open(args.infile), delimiter='\t')
+csv.field_size_limit(longest_line)
+for line in reader:
     counter += 1
+    print counter
     if counter % (1000*1000) == 0:
         print "This many photos processed: %s" % str(counter)
 
     try:
         date = datetime.datetime.strptime(line[5][0:10], '%Y-%m-%d').date()
     except:
-        print "skipping row because failed to parse date: " + str(line)
+        print "skipping row because failed to parse date: " + str(line[5])
         continue
     lon = line[12]
     lat = line[13]
